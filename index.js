6cefbd3f6e52bb996f69fbf5ab981af8d02de91d
Hbib-alt <ongserveau@gmail.com>
2026-05-21 10:01:48 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,542 +1,122 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+exports.get = get;
+exports.getDependencies = getDependencies;
+exports.isInternal = isInternal;
+exports.list = void 0;
+exports.minVersion = minVersion;
+var _t = require("@babel/types");
+var _helpersGenerated = require("./helpers-generated.js");
 const {
-  onDocumentUpdated,
-  onDocumentCreated,
-} = require("firebase-functions/v2/firestore");
-
-const admin = require("firebase-admin");
-
-admin.initializeApp();
-
-// ======================================================
-// CASE APPROVED
-// ======================================================
-
-exports.onCaseApproved = onDocumentUpdated(
-    "cases/{caseId}",
-
-    async (event) => {
-      try {
-        const before =
-        event.data.before.data();
-
-        const after =
-        event.data.after.data();
-
-        if (
-          before.status ===
-          "approved" ||
-
-          after.status !==
-          "approved"
-        ) {
-          return;
-        }
-
-        const userId =
-        after.userId;
-
-        if (!userId) {
-          return;
-        }
-
-        const userDoc =
-        await admin.firestore()
-            .collection("users")
-            .doc(userId)
-            .get();
-
-        const userData =
-        userDoc.data();
-
-        if (!userData) {
-          return;
-        }
-
-        const token =
-        userData.fcmToken;
-
-        if (!token) {
-          return;
-        }
-
-        await admin.messaging().send({
-
-          token: token,
-
-          notification: {
-
-            title:
-            "تمت الموافقة على الطلب",
-
-            body:
-            `تمت الموافقة على طلب "${after.title}"`,
-          },
-
-          data: {
-
-            type: "case",
-
-            caseId:
-            event.params.caseId,
-          },
-
-          android: {
-
-            priority: "high",
-
-            notification: {
-
-              sound:
-              "notification",
-            },
-          },
-        });
-
-        await admin.firestore()
-            .collection(
-                "notifications",
-            )
-            .add({
-
-              userId: userId,
-
-              type: "user",
-
-              title:
-              "تمت الموافقة على الطلب",
-
-              body:
-              `تمت الموافقة على طلب "${after.title}"`,
-
-              createdAt:
-              admin.firestore
-                  .FieldValue
-                  .serverTimestamp(),
-
-              read: false,
-            });
-
-        console.log(
-            "Notification envoyée",
-        );
-      } catch (e) {
-        console.error(
-            "Erreur function:",
-            e,
-        );
+  cloneNode,
+  identifier
+} = _t;
+function deep(obj, path, value) {
+  try {
+    const parts = path.split(".");
+    let last = parts.shift();
+    while (parts.length > 0) {
+      obj = obj[last];
+      last = parts.shift();
+    }
+    if (arguments.length > 2) {
+      obj[last] = value;
+    } else {
+      return obj[last];
+    }
+  } catch (e) {
+    e.message += ` (when accessing ${path})`;
+    throw e;
+  }
+}
+function permuteHelperAST(ast, metadata, bindingName, localBindings, getDependency, adjustAst) {
+  const {
+    locals,
+    dependencies,
+    exportBindingAssignments,
+    exportName
+  } = metadata;
+  const bindings = new Set(localBindings || []);
+  if (bindingName) bindings.add(bindingName);
+  for (const [name, paths] of (Object.entries || (o => Object.keys(o).map(k => [k, o[k]])))(locals)) {
+    let newName = name;
+    if (bindingName && name === exportName) {
+      newName = bindingName;
+    } else {
+      while (bindings.has(newName)) newName = "_" + newName;
+    }
+    if (newName !== name) {
+      for (const path of paths) {
+        deep(ast, path, identifier(newName));
       }
-    },
-);
-
-// ======================================================
-// NEW OPERATION
-// ======================================================
-
-exports.onNewOperation = onDocumentCreated(
-    "operations/{operationId}",
-
-    async (event) => {
-      try {
-        const data =
-        event.data.data();
-
-        if (!data) return;
-
-        if (
-          data.status !==
-          "pending"
-        ) {
-          return;
-        }
-
-        const treasurerQuery =
-        await admin.firestore()
-            .collection("users")
-            .where(
-                "role",
-                "==",
-                "treasurer",
-            )
-            .limit(1)
-            .get();
-
-        if (
-          treasurerQuery.empty
-        ) {
-          return;
-        }
-
-        const treasurerDoc =
-        treasurerQuery.docs[0];
-
-        const treasurerData =
-        treasurerDoc.data();
-
-        const token =
-        treasurerData.fcmToken;
-
-        if (!token) {
-          return;
-        }
-
-        await admin.messaging().send({
-
-          token: token,
-
-          notification: {
-
-            title:
-            "عملية جديدة",
-
-            body:
-            `${data.name} - ${data.amount} أوقية جديدة`,
-          },
-
-          android: {
-
-            priority: "high",
-
-            notification: {
-
-              sound:
-              "notification",
-            },
-          },
-
-          data: {
-
-            type: "operation",
-
-            operationId:
-            event.params.operationId,
-          },
-        });
-
-        await admin.firestore()
-            .collection(
-                "notifications",
-            )
-            .add({
-
-              userId:
-              treasurerDoc.id,
-
-              title:
-              "عملية جديدة",
-
-              body:
-              `${data.name} - ${data.amount} أوقية جديدة`,
-
-              createdAt:
-              admin.firestore
-                  .FieldValue
-                  .serverTimestamp(),
-
-              read: false,
-
-              type: "operation",
-
-              operationId:
-              event.params.operationId,
-
-              createdByName:
-              data.name || "",
-
-              createdByRole:
-              "collector",
-            });
-
-        console.log(
-            "Notification operation envoyée",
-        );
-      } catch (e) {
-        console.error(
-            "Erreur operation:",
-            e,
-        );
+    }
+  }
+  for (const [name, paths] of (Object.entries || (o => Object.keys(o).map(k => [k, o[k]])))(dependencies)) {
+    const ref = typeof getDependency === "function" && getDependency(name) || identifier(name);
+    for (const path of paths) {
+      deep(ast, path, cloneNode(ref));
+    }
+  }
+  adjustAst == null || adjustAst(ast, exportName, map => {
+    exportBindingAssignments.forEach(p => deep(ast, p, map(deep(ast, p))));
+  });
+}
+const helperData = Object.create(null);
+function loadHelper(name) {
+  if (!helperData[name]) {
+    const helper = _helpersGenerated.default[name];
+    if (!helper) {
+      throw Object.assign(new ReferenceError(`Unknown helper ${name}`), {
+        code: "BABEL_HELPER_UNKNOWN",
+        helper: name
+      });
+    }
+    helperData[name] = {
+      minVersion: helper.minVersion,
+      build(getDependency, bindingName, localBindings, adjustAst) {
+        const ast = helper.ast();
+        permuteHelperAST(ast, helper.metadata, bindingName, localBindings, getDependency, adjustAst);
+        return {
+          nodes: ast.body,
+          globals: helper.metadata.globals
+        };
+      },
+      getDependencies() {
+        return Object.keys(helper.metadata.dependencies);
       }
-    },
-);
-
-// ======================================================
-// OPERATION APPROVED
-// ======================================================
-
-exports.onOperationApproved = onDocumentUpdated(
-    "operations/{operationId}",
-
-    async (event) => {
-      try {
-        const before =
-        event.data.before.data();
-
-        const after =
-        event.data.after.data();
-
-        if (
-          before.status ===
-          "approved" ||
-
-          after.status !==
-          "approved"
-        ) {
-          return;
-        }
-
-        const amount =
-        Number(
-            after.amount || 0,
-        );
-
-        if (amount <= 0) {
-          return;
-        }
-
-        const financeRef =
-        admin.firestore()
-            .collection(
-                "finance",
-            )
-            .doc("main");
-
-        await financeRef.update({
-
-          balance:
-          admin.firestore
-              .FieldValue
-              .increment(amount),
-
-          updatedAt:
-          admin.firestore
-              .FieldValue
-              .serverTimestamp(),
-        });
-
-        console.log(
-            `Balance +${amount}`,
-        );
-
-        const userId =
-        after.createdBy;
-
-        if (!userId) {
-          return;
-        }
-
-        const userDoc =
-        await admin.firestore()
-            .collection("users")
-            .doc(userId)
-            .get();
-
-        const userData =
-        userDoc.data();
-
-        if (!userData) {
-          return;
-        }
-
-        const token =
-        userData.fcmToken;
-
-        if (!token) {
-          return;
-        }
-
-        await admin.messaging().send({
-
-          token: token,
-
-          notification: {
-
-            title:
-            "تمت الموافقة على العملية",
-
-            body:
-            `تمت إضافة ${amount} أوقية جديدة إلى الرصيد`,
-          },
-
-          android: {
-
-            priority: "high",
-
-            notification: {
-
-              sound:
-              "notification",
-            },
-          },
-        });
-
-        await admin.firestore()
-            .collection(
-                "notifications",
-            )
-            .add({
-
-              userId:
-              userId,
-
-              title:
-              "تمت الموافقة على العملية",
-
-              body:
-              `تمت إضافة ${amount} أوقية جديدة إلى الرصيد`,
-
-              createdAt:
-              admin.firestore
-                  .FieldValue
-                  .serverTimestamp(),
-
-              read: false,
-            });
-
-        console.log(
-            "Balance updated",
-        );
-      } catch (e) {
-        console.error(
-            "Erreur balance:",
-            e,
-        );
-      }
-    },
-);
-
-// ======================================================
-// NEW TRANSACTION
-// ======================================================
-
-exports.onNewTransaction = onDocumentCreated(
-    "transactions/{transactionId}",
-
-    async (event) => {
-      try {
-        const data =
-        event.data.data();
-
-        if (!data) {
-          return;
-        }
-
-        if (
-          data.paidByRole !==
-          "treasurer"
-        ) {
-          return;
-        }
-
-        const committeeQuery =
-        await admin.firestore()
-            .collection("users")
-            .where(
-                "role",
-                "==",
-                "committee",
-            )
-            .get();
-
-        if (
-          committeeQuery.empty
-        ) {
-          return;
-        }
-
-        for (
-          const doc
-          of committeeQuery.docs
-        ) {
-          const userData =
-          doc.data();
-
-          const token =
-          userData.fcmToken;
-
-          if (!token) {
-            continue;
-          }
-
-          // ================= MESSAGE =================
-
-          const messageBody =
-
-`تم دفع ${data.amount} أوقية جديدة لصالح ${data.beneficiaryName || ""}
-
-الهاتف: ${data.beneficiaryPhone || ""}
-
-الغرض:
-${data.note || "غير محدد"}`;
-
-          // ================= PUSH =================
-
-          await admin.messaging().send({
-
-            token: token,
-
-            notification: {
-
-              title:
-              "تم تنفيذ عملية دفع",
-
-              body:
-              messageBody,
-            },
-
-            android: {
-
-              priority: "high",
-
-              notification: {
-
-                sound:
-                "notification",
-              },
-            },
-
-            data: {
-
-              type:
-              "transaction",
-
-              transactionId:
-              event.params.transactionId,
-            },
-          });
-
-          // ================= FIRESTORE =================
-
-          await admin.firestore()
-              .collection(
-                  "notifications",
-              )
-              .add({
-
-                userId:
-                doc.id,
-
-                title:
-                "تم تنفيذ عملية دفع",
-
-                body:
-                messageBody,
-
-                createdAt:
-                admin.firestore
-                    .FieldValue
-                    .serverTimestamp(),
-
-                read: false,
-              });
-        }
-
-        console.log(
-            "Committee notified",
-        );
-      } catch (e) {
-        console.error(
-            "Erreur transaction:",
-            e,
-        );
-      }
-    },
-);
+    };
+  }
+  return helperData[name];
+}
+function get(name, getDependency, bindingName, localBindings, adjustAst) {
+  if (typeof bindingName === "object") {
+    const id = bindingName;
+    if ((id == null ? void 0 : id.type) === "Identifier") {
+      bindingName = id.name;
+    } else {
+      bindingName = undefined;
+    }
+  }
+  return loadHelper(name).build(getDependency, bindingName, localBindings, adjustAst);
+}
+function minVersion(name) {
+  return loadHelper(name).minVersion;
+}
+function getDependencies(name) {
+  return loadHelper(name).getDependencies();
+}
+function isInternal(name) {
+  var _helpers$name;
+  return (_helpers$name = _helpersGenerated.default[name]) == null ? void 0 : _helpers$name.metadata.internal;
+}
+exports.ensure = name => {
+  loadHelper(name);
+};
+const list = exports.list = Object.keys(_helpersGenerated.default).map(name => name.replace(/^_/, ""));
+var _default = exports.default = get;
+
+//# sourceMappingURL=index.js.map
